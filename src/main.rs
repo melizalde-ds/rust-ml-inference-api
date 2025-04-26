5bcb9dc79ce9c3dcdd34ac9c47969a48aa9a2eba
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2025-04-26 13:04:57 -0600

    feat: add ONNX model loading support

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
+mod inference;
+mod models;
+
+use crate::inference::load_session;
 use axum::routing::get;
 use axum::Router;
+use ort::session::Session;
 use std::net::SocketAddr;
+use std::sync::Arc;
 use tokio::net::TcpListener;
+
+pub struct AppState {
+    pub session: Arc<Session>,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let session = load_session("mnist-8.onnx".to_string())?;
+    let state = AppState {
+        session: Arc::new(session),
+    };
+
     let app = Router::new().route("/", get(hello_world).fallback(get(anything_else)));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let listener = TcpListener::bind(addr).await?;
